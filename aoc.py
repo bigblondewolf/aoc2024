c1fdd11a195ee477c7b4051b6bdca5c99624494d
Ognyan Manchev <ognyan.manchev@gmail.com>
2024-12-03 00:24:01 +0200

    Added convenience functions for reading input in various formats.

--- aoc.py
+++ aoc.py
@@ -12,3 +12,27 @@ def read_example():
     with open(input_fn) as infile:
         data_in = infile.readlines()
     return data_in
+
+def read_input_aslist():
+    input_fn = os.path.basename(sys.argv[0])[:5] + ".txt"
+    with open(input_fn) as infile:
+        data_in = infile.readlines()
+    return [x.split() for x in data_in]
+
+def read_example_aslist():
+    input_fn = os.path.basename(sys.argv[0])[:5] + ".txt.example"
+    with open(input_fn) as infile:
+        data_in = infile.readlines()
+    return [x.split() for x in data_in]
+
+def read_input_aslist_parseints():
+    input_fn = os.path.basename(sys.argv[0])[:5] + ".txt"
+    with open(input_fn) as infile:
+        data_in = infile.readlines()
+    return [[int(y) for y in x.split()] for x in data_in]
+
+def read_example_aslist_parseints():
+    input_fn = os.path.basename(sys.argv[0])[:5] + ".txt.example"
+    with open(input_fn) as infile:
+        data_in = infile.readlines()
+    return [[int(y) for y in x.split()] for x in data_in]
