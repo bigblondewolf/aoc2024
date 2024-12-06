7f3956edf3c8add8ff8134545053aec7d3f0aba0
Ognyan Manchev <ognyan.manchev@gmail.com>
2024-12-07 00:09:19 +0200

    Lib strips lines and can split strings to lists of chars.

--- aoc.py
+++ aoc.py
@@ -5,25 +5,29 @@ def read_input():
     input_fn = os.path.basename(sys.argv[0])[:5] + ".txt"
     with open(input_fn) as infile:
         data_in = infile.readlines()
-    return data_in
+    return [r.strip() for r in data_in]
 
 def read_example():
     input_fn = os.path.basename(sys.argv[0])[:5] + ".txt.example"
     with open(input_fn) as infile:
         data_in = infile.readlines()
-    return data_in
+    return [r.strip() for r in data_in]
 
 def read_input_aslist():
     input_fn = os.path.basename(sys.argv[0])[:5] + ".txt"
     with open(input_fn) as infile:
         data_in = infile.readlines()
-    return [x.split() for x in data_in]
+    if " " in data_in[0]:
+        return [x.split() for x in data_in]
+    return [list(x.strip()) for x in data_in]
 
 def read_example_aslist():
     input_fn = os.path.basename(sys.argv[0])[:5] + ".txt.example"
     with open(input_fn) as infile:
         data_in = infile.readlines()
-    return [x.split() for x in data_in]
+    if " " in data_in[0]:
+        return [x.split() for x in data_in]
+    return [list(x.strip()) for x in data_in]
 
 def read_input_aslist_parseints():
     input_fn = os.path.basename(sys.argv[0])[:5] + ".txt"
